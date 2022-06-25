8fe66574917d7043844bc7c3ed8d72ee7144fa9b
Tomasz Staniewski <tomislaw2.0@gmail.com>
2022-06-25 20:44:03 +0200

    Updated main menu, added menu button assets
    Created main menu where use can select multiple options (start game, forge, shop, credits). Added new assets for menu.

---
+++ Assets/Scripts/Utility/ExitGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace RuinsRaiders
+{
+    public class ExitGame : MonoBehaviour
+    {
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
