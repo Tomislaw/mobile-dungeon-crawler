b61bd39b438b9c12003562d3829af8022b674844
Tomasz Staniewski <tomislaw2.0@gmail.com>
2023-04-04 21:56:49 +0200

    Updated github actions.

--- Assets/Scripts/RuinsRaiders.cs
+++ Assets/Scripts/RuinsRaiders.cs
@@ -5,11 +5,15 @@ namespace RuinsRaiders
     public class App
     {
 
+        #if !UNITY_EDITOR
+
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         static void OnLoad()
         {
             Debug.unityLogger.logHandler = DebugLogger.Instance;
             SaveableData.LoadAll();
         }
+
+        #endif
     }
 }
